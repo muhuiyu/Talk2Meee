6a6628c30e4918b2e18c45c0b8a3e7f317322e10
mlsg-gracemy <grace.yu@monstar-lab.com>
2023-08-18 09:28:32 -0700

    Initial Commit

---
+++ Talk2Meee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talk2Meee
+//
+//  Created by Grace, Mu-Hui Yu on 8/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
